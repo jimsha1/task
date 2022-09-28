445d5c40675fe4045c8859623a9d0fb9b6a9856a
jimshamol5@gmail.com <jimshamol5gmail.com>
2022-09-28 10:32:14 -0700

    first  commit

---
+++ work1.php
@@ -0,0 +1,25 @@
+<?php
+$x=[1,2,3,4,5,6];
+$b=$x[0];
+for($i=1;$i<count($x);$i++)
+{
+    if($b<$x[$i])
+    {
+        $b=$x[$i];
+    }
+}
+    echo " \n large element: ",$b,"\n" ;
+    echo"<br>";
+$sm=$x[0];
+for($i=0;$i<count($x);$i++)
+{
+    if($sm>$x[$i])
+    {
+        $sm=$x[$i];
+    }
+}
+    echo " \n small element: ",$sm,"\n" ;
+
+
+
+?>
